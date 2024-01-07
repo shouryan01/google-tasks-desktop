3f4cd0b1d73b47d32d12128a290024e782aa253f
Shouryan Nikam <snnikam@umich.edu>
2024-01-07 10:09:39 -0800

    Update version to 0.1.1:
    Added the ability to sign out of accounts and added a custom menubar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,64 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::{thread, time::Duration};
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 fn main() {
+    let main_menu = Submenu::new(
+        "File",
+        Menu::new()
+            .add_native_item(MenuItem::About(
+                "Google Tasks".to_string(),
+                Default::default(),
+            ))
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::Hide)
+            .add_native_item(MenuItem::HideOthers)
+            .add_native_item(MenuItem::Separator)
+            .add_item(CustomMenuItem::new("SignOut", "Sign Out"))
+            .add_native_item(MenuItem::Quit),
+    );
+    let window_menu = Submenu::new(
+        "Window",
+        Menu::new()
+            .add_native_item(MenuItem::Minimize)
+            .add_native_item(MenuItem::EnterFullScreen)
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::CloseWindow),
+    );
+    let edit_menu = Submenu::new(
+        "Edit",
+        Menu::new()
+            .add_native_item(MenuItem::Undo)
+            .add_native_item(MenuItem::Redo)
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::Cut)
+            .add_native_item(MenuItem::Copy)
+            .add_native_item(MenuItem::Paste)
+            .add_native_item(MenuItem::SelectAll),
+    );
+
+    let menu = Menu::new()
+        .add_submenu(main_menu)
+        .add_submenu(edit_menu)
+        .add_submenu(window_menu);
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id() {
+            "SignOut" => {
+                let _ = event
+                    .window()
+                    .eval("window.location.href = 'https://mail.google.com/mail/logout?hl=en';");
+
+                let _ = thread::spawn(|| {
+                    thread::sleep(Duration::from_millis(500));
+                    std::process::exit(0x0);
+                });
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,9 @@
 <script>
-	window.location.href = 'https://tasks.google.com/embed/?origin=https://calendar.google.com&fullWidth=1&authuser=snnikam@umich.edu';
+  window.location.href =
+    "https://tasks.google.com/embed/?origin=https://calendar.google.com&fullWidth=1&authuser=snnikam@umich.edu";
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Google Tasks" />
+  <title>Home</title>
+  <meta name="description" content="Google Tasks" />
 </svelte:head>
